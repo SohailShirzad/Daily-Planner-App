2d34e09ebe0a7b0f0ca5d7c91992ff615a5a6a13
sohail <as19adj@herts.ac.uk>
2024-01-23 16:30:07 +0000

    adds time-block html code from 9 AM to 5 PM

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,13 @@
 
 // get the current date element by Id.
 const currentDateElement  = $("#currentDay");
+var c = 0; // counter
 
 // assign current date to the now variable and format it using format method.
 const now = dayjs().format('ddd, MMMM DD, YYYY, h:mm A');
 
 // display it as text 
-currentDateElement.text(now)
+currentDateElement.text(now);
+
+
+// }
